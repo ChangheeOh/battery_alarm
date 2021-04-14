51719042b0b4e1902691d145dcb056001f650ea5
Changhee Oh <changheeoh@gmail.com>
2021-04-14 17:52:44 +0900

    battery_alm modified to exit if fully charged and the adaptor already detached.

--- batt_alm_all.sh
+++ batt_alm_all.sh
@@ -6,13 +6,24 @@ echo $batt_stat '%' $ac_stat $(date)
 
 while true;
 do
+# If battery fully charged and the adaptor already detached, exit
+if [[ ( $batt_stat -gt $2 ) && ( $ac_stat = 'No adapter attached.' ) ]];
+then
+	echo "Exit the battery alarm."
+	say -v Samantha "Exit the battery alarm."
+	say -v Samantha "Exit the battery alarm."
+	break
+fi
+
+# if battery fully charged but adaptor still attached, warn me to detach tha adaptor.
 if [[ ( $batt_stat -gt $2 ) && ( $ac_stat != 'No adapter attached.' ) ]];
 then
-	echo "Fully charged"
+	echo "Fully charged."
 	say -v Samantha "Battery fully charged. Please turn off AC adaptor."
 	say -v Samantha "Battery fully charged. Please turn off AC adaptor."
 fi
 
+# If the battery status is too low and the adaptor not yet attached, warn me to start charging.
 if [[ ( $batt_stat -lt $1 ) && ( $ac_stat == 'No adapter attached.' ) ]];
 then
 
